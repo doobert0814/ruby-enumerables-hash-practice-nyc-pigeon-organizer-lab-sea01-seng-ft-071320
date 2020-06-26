4fc202d18eb2cb9d657bc9439390d9ed6c945c13
Scot Sexton <rss3509@gmail.com>
2020-06-26 23:18:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+   final_results =  data.each_with_object({}) do |(key, values), final_array|
+     #binding.pry
+     values.each do |inner_key, names|
+      names.each do |name|
+      if !final_array[name]
+        final_array [name] = {}
+      end
+      if !final_array[name][key]
+         !final_array[name][key] = []
+     end
+     final_array[name][key].push(inner_key)
+     
+   end
+binding.pry
 end
